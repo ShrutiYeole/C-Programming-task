2242fe999e9c95a31ee961531d672ad414089fb4
ShrutiYeole <93041595+ShrutiYeole@users.noreply.github.com>
2021-10-31 11:55:14 +0530

    Here is a program to initialize a 3x3 array, insert elements into the array ,read and print an array of elements using only pointers. Also find and print the sum of all diagonal elements.

---
+++ matrix.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+int main()
+{
+  int arr[3][3],i,j,sum;
+       printf("------------------------------------------------------\n");  
+       printf("Input elements in the matrix :\n");
+  for(i=0;i<3;i++)
+  {
+      for(j=0;j<3;j++)
+      {
+	      printf("element - [%d],[%d] : ",i,j);
+	      scanf("%d",&arr[i][j]);
+      }
+  }  
+ 
+ printf("\nThe matrix is : \n");
+  for(i=0;i<3;i++)
+  {
+      printf("\n");
+      for(j=0;j<3;j++)
+           printf("%d\t",arr[i][j]);
+  }
+ printf("\n\n");
+ for(i=0;i<3;i++)
+    {
+        for(j=0;j<3;j++)
+        {
+            if(i==j)
+            {
+                sum=sum+arr[i][j];
+            }
+        }
+    }
+ 
+    printf("The sum of diagonal elements of a square matrix = %d\n",sum);
+}
